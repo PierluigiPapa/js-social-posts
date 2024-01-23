3d21db1c6f63d94757bb3eb7c7bce90934b0579e
PierluigiPapa <pierluigipapa9@gmail.com>
2024-01-23 17:06:22 +0100

    feat: add like's array

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,42 +56,47 @@ const posts = [
     }
 ];
 
-//Costante per selezionare la classe nell'html
+//Costante per selezionare la classe staff
 const cardPost = document.querySelector('.post');
 cardPost.innerHTML = '';
 
 //Ciclo per la creazione di un post
 posts.forEach((post) => {
-    cardPost.innerHTML +=`
-    <div class="post">
-            <div class="post__header">
-                <div class="post-meta">                    
-                    <div class="post-meta__icon">
-                        <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
-                    </div>
-                    <div class="post-meta__data">
-                        <div class="post-meta__author">Phil Mangione</div>
-                        <div class="post-meta__time">4 mesi fa</div>
-                    </div>                    
-                </div>
+    cardPost.innerHTML += `<div class="post">
+    <div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="${imageNull(post.author.image)}"  alt="${post.author.name}">                    
             </div>
-            <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
-            <div class="post__image">
-                <img src="https://unsplash.it/600/300?image=171" alt="">
-            </div>
-            <div class="post__footer">
-                <div class="likes js-likes">
-                    <div class="likes__cta">
-                        <a class="like-button  js-like-button" href="#" data-postid="1">
-                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                            <span class="like-button__label">Mi Piace</span>
-                        </a>
-                    </div>
-                    <div class="likes__counter">
-                        Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
-                    </div>
-                </div> 
-            </div>            
+            <div class="post-meta__data">
+                <div class="post-meta__author">${post.author.name}</div>
+                <div class="post-meta__time">${postTime(post.created)}</div>
+            </div>                    
         </div>
-    </div>`
+    </div>
+    <div class="post__text">${post.content}</div>
+    <div class="post__image">
+        <img src="${post.media}" alt="">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="${post.id}">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+            </div>
+        </div> 
+    </div>            
+</div>
+</div>`
+
 })
+
+// Creo un array per vedere i miei like
+const myLike = []
+//Creo un array per vedere i post dove sono stati inseriti i like
+const likedPost = []
